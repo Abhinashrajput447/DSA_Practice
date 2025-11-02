c5fe08dfaf6d5c582d21d0de780323ba020f636e
Abhinashrajput447 <2500030978@kluniversity.in>
2025-11-02 12:47:47 +0530

    modify initial commit

--- Arrayques/reverseArr.cpp
+++ Arrayques/reverseArr.cpp
@@ -4,8 +4,13 @@
 using namespace std;
 
 int main() {
-  int arr[5] = {1,2,3,4,5};
-  int n = 5;
+  int n;
+  cin >> n;
+
+  int arr[n];
+  for(int i=0; i<n; i++) {
+    cin >> arr[i];
+  }
 
   // for(int i=n-1; i>=0; i--) {
   //   cout << arr[i] << " ";
