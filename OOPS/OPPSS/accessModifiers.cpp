fa31fe0d9d09fce112a457b3e70b92264bf4023c
Abhinashrajput447 <2500030978@kluniversity.in>
2025-11-09 02:14:54 +0530

    Access Modifiers pussing on github

---
+++ OOPS/OPPSS/accessModifiers.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+class Student {
+public:
+  string name;
+  int rNo;
+
+  Student(string name, int rNo, float cgpa) {  // paramitirized constuctor
+    this -> name = name;
+    this -> rNo = rNo;
+    this -> cgpa = cgpa;
+  }
+
+  // void display() {
+  //   cout << this -> name << " " << this -> rNo << " " << this -> cgpa << "\n";
+  // }
+
+  float getCgpa() {  
+    return cgpa;
+  }
+
+private:
+  float cgpa;
+};
+
+// 2nd type for printing
+// here wec't access cgpa bcz cgpa in private modifiers
+
+// void display(Student s) {
+//   cout << s.name << " " << s.rNo << " " << s.cgpa << "\n";
+// }
+
+
+int main() {
+  Student s1("Abhinash",21, 9.7);
+  Student s2(s1);
+
+  s2.rNo = 88;
+
+  cout << s1.getCgpa();
+
+  // s1.cgpa();
+  // s1.display();  // insize the class function
+  // display(s1);  // 2nd type
+  return 0;
+}
