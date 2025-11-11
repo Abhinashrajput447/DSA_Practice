e6d08c2e9f1444f594161817880a353ff08e5c57
Abhinashrajput447 <2500030978@kluniversity.in>
2025-11-11 20:26:37 +0530

    pre&in Order Treversal in binary tree pushing on github

--- Binary_Trees/inOrderTreversal.cpp
+++ Binary_Trees/inOrderTreversal.cpp
@@ -8,15 +8,14 @@ public:
   Node* left;
   Node* right;
 
-
-  Node(int val) {  // Constructor
+  Node(int val) {
     data = val;
     left = right = NULL;
   }
 };
 
 static int idx = -1;
-Node* buildTree(vector<int> preorder) {  // O(n)
+Node* buildTree(vector<int> preorder) {   // TC => O(1)
   idx++;
 
   if(preorder[idx] == -1) return NULL;
@@ -28,13 +27,20 @@ Node* buildTree(vector<int> preorder) {  // O(n)
   return root;
 }
 
+void inOrder(Node* root) {  // TC => O(1)
+  if(root == NULL) return;
+
+  inOrder(root -> left);
+  cout << root -> data << " ";
+  inOrder(root -> right);
+}
+
 int main() {
-  vector<int> preorder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
+  vector<int> preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
 
   Node* root = buildTree(preorder);
+  inOrder(root);
 
-  cout << root -> data << endl;
-  cout << root ->left -> data << endl;
-  cout << root ->right -> data << endl;
+  cout << endl;
   return 0;
 }
--- Binary_Trees/preOrderTreaversal.cpp
+++ Binary_Trees/preOrderTreaversal.cpp
@@ -1,5 +1,5 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
 class Node {
@@ -8,33 +8,39 @@ public:
   Node* left;
   Node* right;
 
-
-  Node(int val) {  // Constructor
+  Node(int val) {
     data = val;
     left = right = NULL;
   }
+
 };
 
 static int idx = -1;
-Node* buildTree(vector<int> preorder) {  // O(n)
+Node* buildTree(vector<int> preorder) {    // TC => O(1) 
   idx++;
-
   if(preorder[idx] == -1) return NULL;
-
   Node* root = new Node(preorder[idx]);
   root -> left = buildTree(preorder);
   root -> right = buildTree(preorder);
 
   return root;
+};
+
+
+void preOrder(Node* root) {  // TC => O(1) 
+  if(root == NULL) return;
+
+  cout << root -> data << " ";
+  preOrder(root -> left);
+  preOrder(root -> right);
 }
 
 int main() {
-  vector<int> preorder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
+  vector<int> preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
 
   Node* root = buildTree(preorder);
 
-  cout << root -> data << endl;
-  cout << root ->left -> data << endl;
-  cout << root ->right -> data << endl;
+  preOrder(root);
+  cout << endl;
   return 0;
 }
