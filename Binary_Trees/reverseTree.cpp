64f3ba53cb8637f0c17124ed7e5927d89bc5380c
Abhinashrajput447 <2500030978@kluniversity.in>
2025-11-12 17:28:06 +0530

    reverse preOrder treversal pushing on github

--- Binary_Trees/reverseTree.cpp
+++ Binary_Trees/reverseTree.cpp
@@ -20,12 +20,23 @@ Node* buildTree(vector<int>& preorder) {
   if(preorder[idx] == -1) return NULL;
 
   Node* root = new Node(preorder[idx]);
+  root -> left = buildTree(preorder);
+  root -> right  = buildTree(preorder);
+}
 
+void revPreTree(Node* root) {
+  if(root == NULL) return;
+
+  revPreTree(root -> right);
+  revPreTree(root -> left);
   cout << root -> data << " ";
-  root -> left = preorder()
+
 }
 
 int main() {
+  vector<int> preorder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
+  Node* root = buildTree(preorder);
 
+  revPreTree(root);
   return 0;
 }
