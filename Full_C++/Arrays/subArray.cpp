a3f294d1ba94dbbe0a060e4e24a688b867b95eeb
Abhinashrajput447 <2500030978@kluniversity.in>
2025-11-22 13:26:40 +0530

    subArray code pushing on github

---
+++ Full_C++/Arrays/subArray.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+void printSubArrays(int *arr, int n) {
+  for(int i=0; i<n; i++) {
+    for(int j=i; j<n; j++) {
+      for(int k=i; k<=j; k++) {
+        cout << arr[k];
+      }
+      cout << ",";
+    }
+    cout << "\n";
+  }
+}
+
+int main() {
+  int n = 5;
+  int arr[n] = {1,2,3,4,5};
+
+  printSubArrays(arr, n);
+  return 0;
+}
