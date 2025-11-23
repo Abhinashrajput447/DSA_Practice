7398b2f6d3fb64b48d1d9927189bcf3b225628b9
Abhinashrajput447 <2500030978@kluniversity.in>
2025-11-23 14:33:18 +0530

    diffresnt type to solve best buy and sell the stock

--- Full_C++/Arrays/stockBuySell.cpp
+++ Full_C++/Arrays/stockBuySell.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<limits>
 using namespace std;
 
 int maxProfit(int *arr, int n) {
@@ -15,10 +16,24 @@ int maxProfit(int *arr, int n) {
   return maxPro;
 }
 
+int maxProfit2(int *arr, int n) {
+  int bestBuy[100000];
+  bestBuy[0] = INT32_MAX;
+  int maxProfit = 0;
+
+  for(int i=1; i<n; i++) {
+    bestBuy[i] = min(bestBuy[i-1], arr[i-1]);
+  }
+  for(int i=0; i<n; i++) {
+    maxProfit = max(maxProfit, arr[i]-bestBuy[i]);
+  }
+  return maxProfit;
+}
+
 
 int main() {
   int n = 6;
   int arr[n] = {7, 1, 5, 3, 6, 4};
-  cout << maxProfit(arr, n);
+  cout << maxProfit2(arr, n);
   return 0;
 }
