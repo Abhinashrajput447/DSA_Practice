25b3e9c52e6f48d6833a3612e99ac12a2fc457cf
Abhinashrajput447 <2500030978@kluniversity.in>
2025-11-24 15:37:46 +0530

    inbuilt and charSort code pushing on github

---
+++ Full_C++/Arrays/inbuiltSort.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+void print(int *arr, int n) {
+  for(int i=0; i<n; i++) {
+    cout << arr[i] << " ";
+  }
+}
+
+int main() {
+  int arr[5] = {4, 2, 5, 1, 3};
+  sort(arr+2, arr+5, greater<int>());
+  print(arr, 5);
+  return 0;
+}
