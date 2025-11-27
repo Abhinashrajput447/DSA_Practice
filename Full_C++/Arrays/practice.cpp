37ed200d1b1c7cf28165f4ba39951d642d735a5c
Abhinashrajput447 <2500030978@kluniversity.in>
2025-11-28 00:36:02 +0530

    mostImportant question for interview of view pushing on github

--- Full_C++/Arrays/practice.cpp
+++ Full_C++/Arrays/practice.cpp
@@ -1,21 +1,55 @@
-#include<iostream>
+#include <iostream>
+#include <chrono>
 using namespace std;
 
 int main() {
-  int n;cin >> n;
-  for(int i=1; i<=n; i++) {
-      if(15&i == 15) {
-      cout << "fizzBuss" << endl;
-    }else if(i^3==i) {
-      cout << "fizz" << endl;
-    }
-    else if(i^5==i) {
-      cout << "buzz" << endl;
-    }
-    else{
-      cout << i << endl;
-    }
-  }
+    auto start = chrono::high_resolution_clock::now();
 
-  return 0;
+    int n;
+    cin >> n;
+
+    // for(int i=1; i<=n; i++) {
+    //   if(i % 3 == 0 && i % 5 == 0) {
+    //     // cout << "fizzBUzz" << endl;
+    //   }else if(i % 3 == 0) {
+    //     // cout << "fizz" << endl;
+    //   }else if(i % 5 == 0) {
+    //     // cout << "Buzz" << endl;
+    //   }else{
+    //     // cout << i << endl;
+    //   }
+    // }
+    // int n;
+    // cin >> n;
+
+    // int c3 = 0, c5 = 0;
+
+    for(int i = 1; i <= n; i++) {
+        c3++;
+        c5++;
+
+        if(c3 == 3 && c5 == 5) {
+            // cout << "fizzbuzz" << endl;
+            c3 = 0;
+            c5 = 0;
+        }
+        else if(c5 == 5) {
+            // cout << "buzz" << endl;
+            c5 = 0;
+        }
+        else if(c3 == 3) {
+            // cout << "fizz" << endl;
+            c3 = 0;
+        }
+        else {
+            // cout << i << endl;
+        }
+    }
+
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<double> elapsed = end - start;
+
+    cout << "\nTime taken: " << elapsed.count() << " seconds\n";
+
+    return 0;
 }
