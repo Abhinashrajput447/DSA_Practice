5b3cd414f92ea3f35c9f4e36bd4f510d170c1733
Abhinashrajput447 <2500030978@kluniversity.in>
2025-11-28 19:40:35 +0530

    dynamic memory allocation in two D array

--- Full_C++/Vectors/dynamicMem.cpp
+++ Full_C++/Vectors/dynamicMem.cpp
@@ -13,15 +13,16 @@ void func2() {
   int size;
   cin >> size;
 
-  int *arr = new int(size);
+  int *arr = new int[size];
   int a = 1;
   for(int i=0; i<size; i++) {
     arr[i] = a;
     cout << arr[i] << " ";
     a++;
   }
-  cout << endl << size;
-  delete [] arr; // free space
+  // cout << endl << size;
+  delete[] arr; // free space
+  cout << size;
 }
 
 void func3() {
@@ -30,7 +31,38 @@ void func3() {
   cout << *ptr;
   delete ptr;
 }
+
+int* func4() {
+  int *ptr = new int;
+  *ptr = 1200;
+  cout << "ptr points to " << *ptr << endl;
+  return ptr;
+}
+
+
+// 2D Dynamic Arrays 
+void twoDdynamic(int rows, int cols) {
+  int* *matrix = new int*[rows];
+
+  for(int i=0; i<rows; i++) {
+    matrix[i] = new int[cols];
+  }
+
+  int x = 1;
+  for(int i=0; i<rows; i++) {
+    for(int j=0; j<cols; j++) {
+      matrix[i][j] = x++;
+      cout << matrix[i][j] << " ";
+    }
+    cout << endl;
+  }
+  cout << matrix[2][1] << "\n";
+  cout << *(*(matrix+2)+1) << endl;
+}
+
 int main() {
-  func3();
-  
+  int rows, cols;
+  cin >> rows >> cols;
+  twoDdynamic(rows, cols);
+  return 0;
 }
