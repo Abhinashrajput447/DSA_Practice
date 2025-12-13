63a7fea1cf3b9760bb7d6b829e24844e58d7dd36
Abhinashrajput447 <2500030978@kluniversity.in>
2025-12-14 00:10:38 +0530

    nqueen code pushing on github

---
+++ Full_C++/Arrays/revesion.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+// chaeck duplicate is present or not 
+bool check(vector<int>& nums) {
+  int n = nums.size();
+  sort(nums.begin(), nums.end());
+
+  for(int i=1; i<n; i++) {
+    if(nums[i-1] == nums[i]){
+      return true;
+    }
+  }
+  return false;
+}
+
+bool check2(vector<int>& nums) {
+  unordered_set<int> s;
+
+  for(int x : nums) {
+    if(s.count(x)) {
+      return true;
+    }
+    s.insert(x);
+  }
+  return false;
+}
+
+
+// using unorderd map
+
+void ck(vector<int>& nums) {
+  unordered_map<int, int> map;
+
+  for(int x : nums) {
+    map[x]++;
+  }
+
+  for(auto &p : map) {
+    cout << p.first << " -> " << p.second << endl;
+  }
+  // cout << endl;
+}
+
+int main() {
+  // vector<int> nums = {1, 2, 3, 4};
+  vector<int> nums = {1, 2,3, 1, 5, 6, 7};
+  // cout << check2(nums);
+  ck(nums);
+  return 0;
+}
--- Recursion/RecPracticeQue/NQueens.cpp
+++ Recursion/RecPracticeQue/NQueens.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 void printBoard(vector<vector<char>> board) {
-  int n = 2;
+  int n = board.size();
 
   for(int i=0; i<n; i++) {
     for(int j=0; j<n; j++) {
@@ -12,12 +12,43 @@ void printBoard(vector<vector<char>> board) {
     }
     cout << endl;
   }
-  cout << "-----------" << endl;
+  cout << "--------" << endl;
 }
 
-// bool isSafe(vector<vector<char>> board, int row, int col) {
+bool isSafe(vector<vector<char>> board, int row, int col) {
+  int n = board.size();
 
-// }
+  // safety for horizontally
+  for(int j=0;j<n; j++) {
+    if(board[row][j] == 'Q') {
+      return false;
+    }
+  }
+
+  // safaty for vertically
+  for(int i=0; i<n; i++) {
+    if(board[i][col] == 'Q') {
+      return false;
+    }
+  }
+
+  // check left diognol
+  for(int i=row, j=col; i>=0 && j>=0; i--, j--) {
+    if(board[i][j] == 'Q') {
+      return false;
+    }
+  }
+
+  // check right diognal
+  for(int i=row, j=col; i>=0 && j<n; i--, j++) {
+    if(board[i][j] == 'Q'){
+      return false;
+    }
+    
+  }
+
+  return true;
+}
 
 void nQueens(vector<vector<char>> board, int row) {
   int n = board.size();
@@ -27,19 +58,18 @@ void nQueens(vector<vector<char>> board, int row) {
   }
 
   for(int j=0; j<n; j++) {
-    // if(isSafe(board, row, j)) {
+    if(isSafe(board, row, j)) {
       board[row][j] = 'Q'; // pushing 
       nQueens(board, row+1); // updating 
       board[row][j] = '.'; //  backtracking 
-    // }
-    
+    }
   }
-  
+
 }
 
 int main() {
   vector<vector<char>> board;
-  int n = 2;
+  int n = 4;
 
   for(int i=0; i<n; i++) {
     vector<char> newRow;
