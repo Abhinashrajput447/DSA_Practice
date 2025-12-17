e2fe52821adbdf133ead926d7764d370e85e7435
Abhinashrajput447 <2500030978@kluniversity.in>
2025-12-17 20:43:02 +0530

    pop_front and pop_back code pushing on github

--- LinkedList/CircularLL/linkedList.cpp
+++ LinkedList/CircularLL/linkedList.cpp
@@ -12,16 +12,32 @@ public:
     this -> data = val;
     this -> next = NULL;
   }
+
+  ~Node() { // destrcutor for Node because we created here next using poinr by dynamic memory allocation 
+    if(next != NULL) {
+      // cout << "~Node " << data << endl;
+      delete next;
+      next = NULL;
+    }
+  }
 };
 
 class List {
 public:
   Node* head;
   Node* tail;
-
-  List() {
+  
+  List() { // constructor 
     head = NULL;
     tail = NULL;
+  }
+
+  ~List() { // destructor for deleting dynamic mem created 
+    // cout << "~list \n"; 
+    if(head != NULL) {
+      delete head;
+      head = NULL;
+    }
   }
 
   void push_front(int val) {
@@ -46,16 +62,16 @@ public:
     }
   }
  
-  // void printList() {
-  //   Node* temp = head;
+  void printList() {
+    Node* temp = head;
 
-  //   while(temp != NULL) {
-  //     cout << temp -> data << " -> ";
-  //     temp = temp -> next;
-  //   }
+    while(temp != NULL) {
+      cout << temp -> data << " -> ";
+      temp = temp -> next;
+    }
 
-  //   cout << "NULL\n";
-  // }
+    cout << "NULL\n";
+  }
 
   void printList2() {
     while(head != NULL) {
@@ -81,6 +97,33 @@ public:
     newNode -> next = prev -> next;
     prev -> next = newNode;
   }
+
+  void pop_front() {
+    if(head == NULL) {
+      cout << "LL is empty\n";
+      return;
+    }
+    Node* temp = head;
+    head = (*head).next;
+    temp -> next = NULL;
+    delete temp;
+  }
+
+  void pop_back() {
+    if(head == NULL) {
+      cout << "LL is empty\n";
+      return;
+    }
+
+    Node* temp = head;
+    while(temp -> next -> next != NULL) {
+      temp = temp -> next;
+    }
+
+    temp -> next = NULL;
+    delete tail;
+    tail = temp;
+  }
 };
 
 
@@ -90,8 +133,9 @@ int main() {
   ll.push_back(20);
   ll.push_back(10);
   ll.push_back(5);
-  ll.push_back(10);
-  ll.insert(100, 2);
-  ll.printList2();
+  // ll.push_back(10);
+  // ll.insert(100, 2);
+  ll.pop_back();
+  ll.printList();
   return 0;
 }
