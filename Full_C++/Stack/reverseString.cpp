0cfc629148ac44e9739ad23ad53d04e106da0d81
Abhinashrajput447 <2500030978@kluniversity.in>
2025-12-29 14:28:55 +0530

    reverse string using stack

---
+++ Full_C++/Stack/reverseString.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+string reverseStr(string& str) {
+  string ans;
+  stack<char> s;
+
+  for(int i=0; i<str.size(); i++) {
+    s.push(str[i]);
+  }
+
+  while(!s.empty()) {
+    char top = s.top();
+    ans += top;
+    s.pop();
+  }
+
+  return ans;
+}
+
+int main() {
+  string str = "abcd";
+  cout << reverseStr(str);
+  return 0;
+}
