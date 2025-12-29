47e5e0217af3d7d4535f7e43693b3e5f1312c8ef
Abhinashrajput447 <2500030978@kluniversity.in>
2025-12-29 14:15:23 +0530

    In stack push At bottom code pushing on github

--- Full_C++/Stack/basic.cpp
+++ Full_C++/Stack/basic.cpp
@@ -1,35 +1,36 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 using namespace std;
 
-class Stack {
-    vector<int> v;
-public:
-    void push(int val) {
-        v.push_back(val);
-    }
+// class Stack {
+//     vector<int> v;
+// public:
+//     void push(int val) {
+//         v.push_back(val);
+//     }
 
-    void pop() {
-        if(isEmpty()) {
-            cout << "stack is empty \n";
-            return;
-        }
-        v.pop_back();
-    }
-    int top() {
-        if(isEmpty()) {
-            cout << "stack is empty \n";
-            return -1;
-        }
-        return v[v.size()-1];
-    }
+//     void pop() {
+//         if(isEmpty()) {
+//             cout << "stack is empty \n";
+//             return;
+//         }
+//         v.pop_back();
+//     }
+//     int top() {
+//         if(isEmpty()) {
+//             cout << "stack is empty \n";
+//             return -1;
+//         }
+//         return v[v.size()-1];
+//     }
 
-    // condition check stack is empty or not
+//     // condition check stack is empty or not
 
-    bool isEmpty() {
-        return v.size() == 0;
-    }
-};
+//     bool isEmpty() {
+//         return v.size() == 0;
+//     }
+// };
 
 // class Stack1 {
 //     int arr[10];
@@ -41,12 +42,12 @@ public:
 // };
 
 int main() {
-    Stack s;
+    stack<int> s;
     s.push(10);
     s.push(20);
     s.push(30);
 
-    while(!s.isEmpty()) {
+    while(!s.empty()) {
         cout << s.top() << " ";
         s.pop();
     }
