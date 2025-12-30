93d59eacee2f296d3b70f6ce928a38a58c4a6b20
Abhinashrajput447 <2500030978@kluniversity.in>
2025-12-30 22:30:43 +0530

    duplicate parantheses code pushing github

--- Full_C++/Stack/validParentheses.cpp
+++ Full_C++/Stack/validParentheses.cpp
@@ -29,8 +29,31 @@ bool validParan(string& str) {
   return s.empty();
 }
 
+bool duplicateParan(string str) {
+  stack<char> s;
+
+  for(int i=0; i<str.size(); i++) {
+    char ch = str[i];
+
+    if(ch != ')') {
+      s.push(ch);
+
+    }else{
+      if(s.top() == '(') {
+        return true;
+      }
+
+      while(s.top() != '(') {
+        s.pop();
+      }
+      s.pop();
+    }
+  }
+  return false;
+}
+
 int main() {
-  string s1 = "(())()";
-  cout << validParan(s1);
+  string s1 = "((a+b))";
+  cout << duplicateParan(s1);
   return 0;
 }
