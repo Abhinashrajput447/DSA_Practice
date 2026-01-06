e8c382e7ef5faa6db49a31b93c9905c2aadfb201
Abhinashrajput447 <2500030978@kluniversity.in>
2026-01-06 19:04:52 +0530

    find minimum code pushing on github

--- Full_C++/Revision/AllInOne.cpp
+++ Full_C++/Revision/AllInOne.cpp
@@ -195,7 +195,7 @@ int sumOfSpeSqNo(vector<int>& nums) {
   return ans;
 }
 
-int searchInRotSorArr(vector<int>& nums) {
+int findMinInRotSorArr(vector<int>& nums) {
   int n = nums.size();
   int minimum = INT32_MAX;
   int st=0, end=n-1;
@@ -219,8 +219,37 @@ int searchInRotSorArr(vector<int>& nums) {
   return minimum;
 }
 
+int searchInRotSorArr(vector<int>& nums, int tar) {
+  int n = nums.size();
+  int st=0, end=n-1;
+
+  while(st <= end) {
+    int mid = st+(end-st)/2;
+
+    if(nums[mid] == tar) return mid;
+
+    if(nums[st] <= nums[mid]) {
+      if(nums[st] <= tar && tar <= nums[mid]) {
+        end = mid-1;
+      }else{
+        st = mid+1;
+      }
+    }else{
+      if(nums[end] >= nums[mid]) {
+        if(nums[mid] <= tar && tar <= nums[end]) {
+          st = mid+1;
+        }else{
+          end = mid-1;
+        }
+      }
+    }
+  }
+  return -1;
+}
+
+
 int main() {
   vector<int> nums = {7,8,9,10,10,1,2,3};
-  cout << searchInRotSorArr(nums);
+  cout << searchInRotSorArr(nums, 11);
   return 0;
 }
